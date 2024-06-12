26a350c1a284a8e8d114216773903c0864398b10
王俊杰 <wangjunjie@xsyxsc.com>
2024-06-12 15:14:38 +0800

    create venv environment

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import os
+import urllib.request
 
+if not os.path.exists("../Data/the-verdict.txt"):
+    url = ("https://raw.githubusercontent.com/rasbt/"
+           "LLMs-from-scratch/main/ch02/01_main-chapter-code/"
+           "the-verdict.txt")
+    file_path = "the-verdict.txt"
+    urllib.request.urlretrieve(url, file_path)
+
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+print("Total number of character:", len(raw_text))
+print(raw_text[:99])
+
+import re
+
+text = "Hello, world. This, is a test."
+result = re.split(r'(\s)', text)
+
+print(result)
